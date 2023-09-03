b6d8f80d75f0144da0dff5b30659106cea2f80ea
NafJade <nafishayanur@gmail.com>
2023-09-03 23:54:03 +0600

    Header,sidebar,main

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-open-btn').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.add('open');
+});
+
+document.querySelector('.menu-close-btn').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.remove('open');
+ });
+ 
